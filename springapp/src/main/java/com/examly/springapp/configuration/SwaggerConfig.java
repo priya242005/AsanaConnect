19f67f48da438fd7587a8a24be9abd4778600575
priya242005 <priya02april2005@gmail.com>
2025-08-06 19:32:19 +0530

    commit8

---
+++ springapp/src/main/java/com/examly/springapp/configuration/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.examly.springapp.configuration;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Yoga Instructor Application API")
+                        .version("1.0")
+                        .description("API documentation for the Yoga Instructor App"));
+    }
+}
